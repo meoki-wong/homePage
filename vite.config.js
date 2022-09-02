7243a20ed722889c062802f3986921dd11977adc
wangyongfeng <1084415961@qq.com>
2022-09-03 00:13:39 +0800

    fix: vite打包优化

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,12 @@ import analyze from 'rollup-plugin-analyzer';
 import requireTransform from 'vite-plugin-require-transform';
 import replace from '@rollup/plugin-replace';
 import commonjs from '@rollup/plugin-commonjs'
+import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
+        commonjs(),
+        react(),
         reactRefresh(),
         antdDayjs(),
         analyze({   // 用户分析包的大小 和 数量
@@ -15,15 +18,21 @@ export default defineConfig({
             limit: 10, //
         }),
         replace({
-            "process.env.NODE_ENV": JSON.stringify("development")
+            "process.env.NODE_ENV": JSON.stringify("production")
         }),
+        
         requireTransform({
             fileRegex:/.ts$|.tsx$|.vue$|.js$/
           //   fileRegex:/.js$|.jsx$|.vue$/
           }),
-          commonjs()
+          
           
     ],
+    optimizeDeps: {
+          exclude: ['node_modules'],
+          include: []
+        // commonjsOptions: { exclude: ['shared-project'], include: [] }
+   },
     base: './',
     resolve: {
         extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
