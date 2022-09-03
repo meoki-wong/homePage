78cbde8bae383e910c35f034b50d2f4ab1cc423c
wangyongfeng <1084415961@qq.com>
2022-09-04 00:57:36 +0800

    --

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,13 @@ import antdDayjs from 'antd-dayjs-vite-plugin';
 import analyze from 'rollup-plugin-analyzer';
 import requireTransform from 'vite-plugin-require-transform';
 import commonjs from '@rollup/plugin-commonjs'
+const react = require('@vitejs/plugin-react').default;
+const pkg = require('./package.json')
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         commonjs(),
+        react(),
         reactRefresh(),
         antdDayjs(),
         analyze({   // 用户分析包的大小 和 数量
@@ -18,6 +21,9 @@ export default defineConfig({
             fileRegex: /.js$|.vue$|.jsx$|.ts$|.tsx$/
           }),
     ],
+    optimizeDeps: {
+        include: Object.keys(pkg.dependencies) // 这里为了方便将所有 dependencies 的包都加上了
+      },
     base: './',
     resolve: {
         extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
@@ -25,7 +31,8 @@ export default defineConfig({
     },
     build: {
         outDir: 'build',
-        minify: 'terser', // boolean | 'terser' | 'esbuild'
+        minify: 'false', // boolean | 'terser' | 'esbuild'
+        cssCodeSplit: true // 拆分css
     },
     server: {
         port: 8000,
