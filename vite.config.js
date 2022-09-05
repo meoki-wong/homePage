9bf44c52d31c62a419dabbde9da694f0a21e8d79
wangyongfeng <1084415961@qq.com>
2022-09-05 10:17:09 +0800

    test: vite build

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,18 @@ import antdDayjs from 'antd-dayjs-vite-plugin';
 import analyze from 'rollup-plugin-analyzer';
 import requireTransform from 'vite-plugin-require-transform';
 import commonjs from '@rollup/plugin-commonjs'
-const react = require('@vitejs/plugin-react').default;
+import react from '@vitejs/plugin-react';
+import vitePluginImp from 'vite-plugin-imp'
 const pkg = require('./package.json')
+import antdViteImportPlugin from 'antd-vite-import-plugin';
+import { createStyleImportPlugin, AntdResolve } from 'vite-plugin-style-import'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
+        // reactRefresh(),
         commonjs(),
-        react(),
-        reactRefresh(),
+        // react(),
+        antdViteImportPlugin(),
         antdDayjs(),
         analyze({   // 用户分析包的大小 和 数量
             summaryOnly: true,
@@ -20,18 +24,36 @@ export default defineConfig({
         requireTransform({
             fileRegex: /.js$|.vue$|.jsx$|.ts$|.tsx$/
           }),
+          vitePluginImp({
+            libList: [
+              {
+                libName: 'antd',
+                style: (name) => `antd/es/${name}/style/index.less`,
+              },
+            ],
+          }),
+          
+        // createStyleImportPlugin({ resolves: [AntdResolve()] })
     ],
-    optimizeDeps: {
-        include: Object.keys(pkg.dependencies) // 这里为了方便将所有 dependencies 的包都加上了
-      },
+    // optimizeDeps: {
+    //     include: Object.keys(pkg.dependencies) // 这里为了方便将所有 dependencies 的包都加上了
+    //   },
     base: './',
     resolve: {
         extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
         alias: [{ find: '@/', replacement: '/src/' }],
     },
+    css: {
+        preprocessorOptions: {
+          less: {
+            // modifyVars: { 'primary-color': '#13c2c2' },
+            javascriptEnabled: true,
+          },
+        },
+      },      
     build: {
         outDir: 'build',
-        minify: 'false', // boolean | 'terser' | 'esbuild'
+        minify: 'esbuild', // boolean | 'terser' | 'esbuild'
         cssCodeSplit: true // 拆分css
     },
     server: {
