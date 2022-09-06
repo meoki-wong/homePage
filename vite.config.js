e25b598c506917b6adc828e5b92d81f760d77d50
wangyongfeng <1084415961@qq.com>
2022-09-06 17:50:09 +0800

    feat: 打包体积优化

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
-import vitePluginImp from 'vite-plugin-imp'
+// import vitePluginImp from 'vite-plugin-imp'
 import analyze from 'rollup-plugin-analyzer';
 import {createStyleImportPlugin, AntdResolve } from 'vite-plugin-style-import';
 // import commonjs from '@rollup/plugin-commonjs'
@@ -10,6 +10,7 @@ export default defineConfig({
   plugins: [
     // commonjs(),
     react(),
+    splitVendorChunkPlugin(),
     createStyleImportPlugin({
       resolves: [AntdResolve()]
   }),
@@ -33,9 +34,31 @@ export default defineConfig({
     },
   },   
   build: {
+    // sourcemap: "inline",
     outDir: 'build',
-    minify: 'esbuild', // boolean | 'terser' | 'esbuild'
-    cssCodeSplit: true // 拆分css
+    assetsDir: 'static/img/', // 指定生成静态资源的存放路径
+    minify: 'terser', // boolean | 'terser' | 'esbuild'
+    cssCodeSplit: true, // 拆分css
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if(id.includes('node_modules')){
+            // return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            const arr = id.toString().split('node_modules/')[1].split('/')
+            // console.log('-----arr', arr[0])
+            switch(arr[0]){
+              case "dexie": 
+              return arr[0]
+              break
+
+            }
+          };
+        },
+          chunkFileNames: 'static/js1/[name]-[hash].js',
+          entryFileNames: 'static/js2/[name]-[hash].js',
+          assetFileNames: 'static/[ext]/[name]-[hash].[ext]'
+      }
+    }
 },
 server: {
     port: 8000
