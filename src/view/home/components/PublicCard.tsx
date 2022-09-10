ede733d473a4e221c583f06ffa626cc2686859aa
wangyongfeng <1084415961@qq.com>
2022-09-11 00:03:13 +0800

    feat: 开发首页

--- src/view/home/components/PublicCard.tsx
+++ src/view/home/components/PublicCard.tsx
@@ -3,7 +3,7 @@ import "../assets/css/PublicCard.less"
 export default function PublicCard() {
   return (
     <div className='pub-contain'>
-        84932489
+        <div className="master-line"></div>
     </div>
   )
 }
