31be8d3146b3b308c64d9833be57ceda72b48e16
wangyongfeng <1084415961@qq.com>
2022-09-19 15:06:12 +0800

    feat: 统计看板开发

--- src/view/home/components/DataStatistics.tsx
+++ src/view/home/components/DataStatistics.tsx
@@ -1,86 +1,76 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Chart } from "@antv/g2";
 import "../assets/css/dataStatistics.less"
+import { request } from "@/api/request";
+import { Data } from "@antv/g2/lib/interface";
 export default function DataStatistics() {
     useEffect(()=>{
-        const chart = new Chart({
-            container: "container",
-            autoFit: true,
-            height: 200,
-            padding: [30, 20, 70, 30],
-          });
-          const data = [
-            {
-              "date": "2015-01-04",
-              "lastYear": 0,
-              "thisYears": 14
-            },
-            {
-              "date": "2015-01-11",
-              "lastYear": 1,
-              "thisYears": 12
-            },
-            {
-              "date": "2015-01-18",
-              "lastYear": 2,
-              "thisYears": 43
-            },
-            {
-              "date": "2015-01-25",
-              "lastYear": 13,
-              "thisYears": 33
-            },
-          ]
-          chart.data(data);
-          chart.scale({
-            lastYear: {
-              min: 0,
-              max: 100,
-            },
-            thisYears: {
-              min: 0,
-              max: 100,
-            },
-          });
-        
-          chart.axis("lastYear", false);
-        
-          chart.legend({
-            custom: true,
-            items: [
-              {
-                name: "thisYears",
-                value: "thisYears",
-                marker: { symbol: "line", style: { stroke: "#1890ff", lineWidth: 2 } },
-              },
-              {
-                name: "lastYear",
-                value: "lastYear",
-                marker: { symbol: "line", style: { stroke: "#2fc25b", lineWidth: 2 } },
-              },
-            ],
-          });
-        
-          chart.line().position("date*thisYears").color("#1890ff");
-          chart.line().position("date*lastYear").color("#2fc25b");
-        
-          chart.annotation().dataMarker({
-            top: true,
-            position: ["2016-02-28", 9],
-            text: {
-              content: "今年 超过 去年",
-              style: {
-                textAlign: "left",
-              },
-            },
-            line: {
-              length: 30,
-            },
-          });
-          chart.removeInteraction("legend-filter"); // 自定义图例，移除默认的分类图例筛选交互
-          chart.render();
+      getData()
+          
     }, [])
-  
+    const initChart = (chartData: Data)=>{
+      const chart = new Chart({
+        container: "container",
+        autoFit: true,
+        height: 200,
+        padding: [30, 20, 70, 30],
+      });
+      const data = chartData
+      chart.data(data);
+      chart.scale({
+        lastYear: {
+          min: 0,
+          max: 100,
+        },
+        thisYears: {
+          min: 0,
+          max: 100,
+        },
+      });
+    
+      chart.axis("lastYear", false);
+    
+      chart.legend({
+        custom: true,
+        items: [
+          {
+            name: "thisYears",
+            value: "thisYears",
+            marker: { symbol: "line", style: { stroke: "#1890ff", lineWidth: 2 } },
+          },
+          {
+            name: "lastYear",
+            value: "lastYear",
+            marker: { symbol: "line", style: { stroke: "#2fc25b", lineWidth: 2 } },
+          },
+        ],
+      });
+    
+      chart.line().position("date*thisYears").color("#1890ff");
+      chart.line().position("date*lastYear").color("#2fc25b");
+    
+      chart.annotation().dataMarker({
+        top: true,
+        position: ["2016-02-28", 9],
+        text: {
+          content: "今年 超过 去年",
+          style: {
+            textAlign: "left",
+          },
+        },
+        line: {
+          length: 30,
+        },
+      });
+      chart.removeInteraction("legend-filter"); // 自定义图例，移除默认的分类图例筛选交互
+      chart.render();
+    }
+    const getData = async () => {
+      let res = await request.get('/getPVStatistics')
+      if(res.data.success){
+        initChart(res.data.data)
+      }
+    }
 
   return (
     <div className="cnzz-box">
--- src/view/home/type/home.d.ts
+++ src/view/home/type/home.d.ts
@@ -4,3 +4,15 @@
 export type DateTime = {
     [key in string]: number
 }
+
+/**
+ * 页面访问量数据
+ * @param date 日期
+ * @param lastYear 去年同期访问量 （7天）
+ * @param thisYears 今年访问量    （7天）
+ */
+export interface ChartData {
+    date: string,
+    lastYear: string,
+    thisYears: string
+}
