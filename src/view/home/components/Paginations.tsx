61102f6b10f2c08f3bdafa9900e3d5ee1a5dc355
wangyongfeng <1084415961@qq.com>
2022-10-31 12:12:01 +0800

    feat: 提交代码

--- src/view/home/components/Paginations.tsx
+++ src/view/home/components/Paginations.tsx
@@ -4,36 +4,41 @@ import "./Paginations.less";
 export default function Paginations(props: PaginationProps) {
 	const { total, onchange } = props;
 
-	const [pageArr, setPageArr] = useState<number[]>([1])
-  let [active, setActive] = useState<number>(1)
-  const calcPageArr = (length: number) => Array.from({length}).map((v, k) => k + 1);
-  useEffect(()=>{
-    setPageArr(calcPageArr(Math.ceil((total || 0) / 10)))
-  }, [total])
-  /*
-   * @param item 显示切换的页码
-   */
+	const [pageArr, setPageArr] = useState<number[]>([1]);
+	let [active, setActive] = useState<number>(1);
+	const calcPageArr = (length: number) =>
+		Array.from({ length }).map((v, k) => k + 1);
+	useEffect(() => {
+		setPageArr(calcPageArr(Math.ceil((total || 0) / 10)));
+	}, [total]);
+	/*
+	 * @param item 显示切换的页码
+	 */
 	const changePage = (item: number) => {
 		onchange && onchange(item, 10);
-    setActive(item)
-    
+		setActive(item);
 	};
-  const prePage = () => {
-    changePage(--active)
-  }
-  const nextPage = () => {
-    changePage(++active)
-  }
-  const goLastPage = () => {
-    changePage(pageArr[pageArr.length-1])
-  }
-  const goFirstPage = () => {
-    changePage(1)
-  }
+	const prePage = () => {
+    console.log('---active', active);
+		if (active > 1) {
+			changePage(--active);
+		}
+	};
+	const nextPage = () => {
+		if (active < pageArr.length) {
+			changePage(++active);
+		}
+	};
+	const goLastPage = () => {
+		changePage(pageArr[pageArr.length - 1]);
+	};
+	const goFirstPage = () => {
+		changePage(1);
+	};
 	return (
 		<div className="pagination-contain">
 			<div className="statistic">
-				第 <span>{total ? 1 : 0}</span> 页 / 共{" "}
+				第 <span>{total ? 1 : 0}</span> 页 / 共
 				<span>{total || 0}</span> 条
 			</div>
 			<ul className="pagination-box">
@@ -41,19 +46,20 @@ export default function Paginations(props: PaginationProps) {
 				<li onClick={prePage}>
 					<i className="iconfont icon-shangyiye"></i>
 				</li>
-				{
-					pageArr?.map((item: number) => {
-						return item !== 3 ? (
-							<li 
-              className={item === active? 'active-page' : ''}
-              onClick={() => changePage(item)}>{item}</li>
-						) : (
-							<li>
-								<i className="iconfont icon-gengduo"></i>
-							</li>
-						);
-					})
-				}
+				{pageArr?.map((item: number) => {
+					return item !== 3 ? (
+						<li
+							className={item === active ? "active-page" : ""}
+							onClick={() => changePage(item)}
+						>
+							{item}
+						</li>
+					) : (
+						<li>
+							<i className="iconfont icon-gengduo"></i>
+						</li>
+					);
+				})}
 				<li onClick={nextPage}>
 					<i className="iconfont icon-xiayiye"></i>
 				</li>
