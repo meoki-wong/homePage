9c635b5a88e60424848b0de1cfdb0306e099bb68
wangyongfeng <wangyf25@asiainfo.com>
2022-12-26 23:51:24 +0800

    feat: 替换节气

--- src/view/home/components/BeautyPhoto.tsx
+++ src/view/home/components/BeautyPhoto.tsx
@@ -17,7 +17,7 @@ export default function BeautyPhoto(props: any) {
   return (
     <div className="beauty-photo">
       <img
-        src="https://hippo-meoki.oss-cn-beijing.aliyuncs.com/homePage/homePage-image/shuangjiang.jpeg"
+        src="https://hippo-meoki.oss-cn-beijing.aliyuncs.com/homePage/homePage-image/dongzhi.jpeg"
         alt=""
       />
       <div className="intro-box">
@@ -27,7 +27,8 @@ export default function BeautyPhoto(props: any) {
         alt="" />
         </div>
         <p className="desc-fest">
-        霜降是秋季的最后一个节气，是秋季到冬季的过渡。霜降时节，万物毕成，毕入于戌，阳下入地，阴气始凝。天气渐寒始于霜降。《逸周书·周月》：“秋三月中气：处暑、秋分、霜降。”
+        冬至，又称日南至、冬节、亚岁等，兼具自然与人文两大内涵，既是二十四节气中一个重要的节气，也是中国民间的传统祭祖节日。冬至是四时八节之一，被视为冬季的大节日，在古代民间有“冬至大如年”的讲法
+        {/* 霜降是秋季的最后一个节气，是秋季到冬季的过渡。霜降时节，万物毕成，毕入于戌，阳下入地，阴气始凝。天气渐寒始于霜降。《逸周书·周月》：“秋三月中气：处暑、秋分、霜降。” */}
         </p>
         </div>
     </div>
